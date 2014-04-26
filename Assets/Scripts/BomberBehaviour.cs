19de0b0c329e6619e6416d9a0fc21ed3e54f457e
Vilya Harvey <vilya.harvey@gmail.com>
2014-04-26 20:59:26 +0100

    Turn bomber into a prefab and add some behaviour

--- Assets/Scripts/BomberBehaviour.cs
+++ Assets/Scripts/BomberBehaviour.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class BomberBehaviour : MonoBehaviour {
+	public float speed;
 
 	// Use this for initialization
 	void Start () {
-	
+		float direction = (Random.value >= 0.5f) ? 1.0f : -1.0f;
+		rigidbody.velocity = transform.right * speed * direction;
 	}
 	
 	// Update is called once per frame
