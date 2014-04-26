f63904272df018bd8f3a8f3b61c573d357f216b9
Vilya Harvey <vilya.harvey@gmail.com>
2014-04-26 18:33:19 +0100

    Add a script to destroy things on contact
    Used for when enemies are hit by a laser.

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag != "Boundary") {
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
+
+}
