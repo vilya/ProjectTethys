430e7e3adca483390bfb4e7fbc0c71036219e3ab
Vilya Harvey <vilya.harvey@gmail.com>
2014-04-26 21:00:24 +0100

    Add a game controller & prevent player leaving the game area
    This fixes issue #1, among other things.

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class DestroyByBoundary : MonoBehaviour {
 
 	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player") {
+			return;
+		}
 		Destroy(other.gameObject);
 	}
 
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject bomber;
+	public GameObject fighter;
+
+	public Vector3 spawnValues;
+
+
+	// Use this for initialization
+	void Start() {
+		SpawnWaves(bomber);
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+
+	void SpawnWaves(GameObject enemy) {
+		for (int i = 0; i < 10; ++i) {
+			Vector3 spawnPosition = new Vector3(
+				Random.Range(-spawnValues.x, spawnValues.x),
+				Random.Range(-spawnValues.y, spawnValues.y),
+				0.0f
+			);
+			Instantiate(enemy, spawnPosition, Quaternion.identity);
+		}
+	}
+}
