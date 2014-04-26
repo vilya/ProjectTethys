916f22cc1dc5ef3bf6cd16ee39697b95432b5caf
Vilya Harvey <vilya.harvey@gmail.com>
2014-04-26 22:27:19 +0100

    Add explosions when you destory an enemy.

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
+	public GameObject explosion;
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag != "Boundary") {
-			Destroy(other.gameObject);
-			Destroy(gameObject);
+		if (other.gameObject.tag == "Boundary") {
+			return;
 		}
+		Instantiate(explosion, gameObject.transform.position, Quaternion.identity);
+		Destroy(other.gameObject);
+		Destroy(gameObject);
 	}
-
 }
