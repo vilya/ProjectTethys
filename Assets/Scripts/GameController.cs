d00109c2ec430a8c35f27788d1828e850cfec5e8
Vilya Harvey <vilya.harvey@gmail.com>
2014-04-27 00:54:46 +0100

    Spawn multiple randomised waves of enemies

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,25 +7,33 @@ public class GameController : MonoBehaviour {
 
 	public Vector3 spawnValues;
 
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
 
 	// Use this for initialization
 	void Start() {
-		SpawnWaves(bomber);
+		StartCoroutine(SpawnWaves(bomber));
 	}
 	
-	// Update is called once per frame
-	void Update() {
-	
-	}
 
-	void SpawnWaves(GameObject enemy) {
-		for (int i = 0; i < 10; ++i) {
-			Vector3 spawnPosition = new Vector3(
-				Random.Range(-spawnValues.x, spawnValues.x),
-				Random.Range(-spawnValues.y, spawnValues.y),
-				0.0f
-			);
-			Instantiate(enemy, spawnPosition, Quaternion.identity);
+	IEnumerator SpawnWaves(GameObject enemy) {
+		// Delay before launching the first wave.
+		yield return new WaitForSeconds(startWait);
+		while (true) {
+			for (int i = 0; i < 10; ++i) {
+				Vector3 spawnPosition = new Vector3(
+					Random.Range(-spawnValues.x, spawnValues.x),
+					Random.Range(-spawnValues.y, spawnValues.y),
+					0.0f
+				);
+				Instantiate(enemy, spawnPosition, Quaternion.identity);
+				// Small delay in between launching each wave.
+				yield return new WaitForSeconds(spawnWait);
+			}
+			// Pause before launching the next wave.
+			yield return new WaitForSeconds(waveWait);
 		}
 	}
 }
