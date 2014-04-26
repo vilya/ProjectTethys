376013478151e0be382bfcc8bd22f918197e5895
Vilya Harvey <vilya.harvey@gmail.com>
2014-04-26 23:09:08 +0100

    Improve player controls and add lights to laser beams

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, yMin, yMax;
+}
+
+
 public class PlayerController : MonoBehaviour {
 	public float speed;
-	public float tilt;
+	public Boundary boundary;
 
 	public GameObject shot;
 	public Transform shotSpawn;
 	public float fireRate;
 	private float nextFire;
 
+	private float maxSpeed;
 
 	void Update() {
 		if (Input.GetButton("Fire1") && Time.time > nextFire) {
@@ -25,13 +33,17 @@ public class PlayerController : MonoBehaviour {
 		float y = Input.GetAxis("Vertical");
 		Vector3 movement = new Vector3(x, y, 0);
 
-		rigidbody.AddForce(movement * speed * Time.deltaTime);
-
+		rigidbody.velocity = movement * speed;
+		rigidbody.position = new Vector3(
+			Mathf.Clamp(rigidbody.position.x, boundary.xMin, boundary.xMax),
+			Mathf.Clamp(rigidbody.position.y, boundary.yMin, boundary.yMax),
+			0.0f
+		);
 		if (x < 0.0f) {
-			rigidbody.MoveRotation(Quaternion.Euler(0.0f, 180.0f, 90.0f + rigidbody.velocity.y * tilt));
+			rigidbody.MoveRotation(Quaternion.Euler(0.0f, 180.0f, 90.0f));
 		}
 		else if (x > 0.0f) {
-			rigidbody.MoveRotation(Quaternion.Euler(0.0f, 0.0f, 90.0f + rigidbody.velocity.y * tilt));
+			rigidbody.MoveRotation(Quaternion.Euler(0.0f, 0.0f, 90.0f));
 		}
 	}
 }
