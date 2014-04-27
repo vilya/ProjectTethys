639f96e941802994cf7f14d09a7cfbebef0848b0
Vilya Harvey <vilya.harvey@gmail.com>
2014-04-27 20:08:23 +0100

    Add a title screen

---
+++ Assets/Scripts/TitleScreenController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Application.LoadLevel("Main");
+		}
+	}
+}
